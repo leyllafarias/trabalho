25cb38cef09c6599465aed0ed10d449dae0c4b12
aluno <aluno@Lab-07>
2020-03-11 14:10:44 -0300

    Interface aprimorada

--- conversão.sh
+++ conversão.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "Bem vindo ao assistente de descompactação de leila andrade"
+
+echo "Iniciando a descompactação"
+
 cd Downloads
 unzip -q imagens-livros.zip
 
@@ -9,3 +13,9 @@ convert *.jpg teste1.png
 mkdir conversao
 mv teste*.png conversao
 zip -r conversao.zip conversao
+
+echo "Pronto"
+
+echo "Certezaaaaa"
+
+
